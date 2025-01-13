5f26a55cb567500b00425dc1f5672bea90cc9923
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2025-01-13 10:34:21 -0500

    add sncloud and sambastudio chat models  to package init

--- langchain_sambanova/__init__.py
+++ langchain_sambanova/__init__.py
@@ -1,12 +1,7 @@
 from importlib import metadata
 
-from langchain_sambanova.chat_models import ChatSambaNova
-from langchain_sambanova.document_loaders import SambaNovaLoader
+from langchain_sambanova.chat_models import ChatSambaStudio, ChatSambaNovaCloud
 from langchain_sambanova.embeddings import SambaNovaEmbeddings
-from langchain_sambanova.retrievers import SambaNovaRetriever
-from langchain_sambanova.toolkits import SambaNovaToolkit
-from langchain_sambanova.tools import SambaNovaTool
-from langchain_sambanova.vectorstores import SambaNovaVectorStore
 
 try:
     __version__ = metadata.version(__package__)
@@ -16,12 +11,8 @@ except metadata.PackageNotFoundError:
 del metadata  # optional, avoids polluting the results of dir(__package__)
 
 __all__ = [
-    "ChatSambaNova",
-    "SambaNovaVectorStore",
+    "ChatSambaNovaCloud",
+    "ChatSambaStudio",
     "SambaNovaEmbeddings",
-    "SambaNovaLoader",
-    "SambaNovaRetriever",
-    "SambaNovaToolkit",
-    "SambaNovaTool",
     "__version__",
 ]
