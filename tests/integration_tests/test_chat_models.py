8d58fdb16bd898c29b7966837d315aa9e9e83b80
snova-jorgep <jorge.piedrahita@sambanovasystems.com>
2025-05-06 12:36:10 -0500

    hot fix

--- tests/integration_tests/test_chat_models.py
+++ tests/integration_tests/test_chat_models.py
@@ -36,27 +36,27 @@ class TestSambaNovaCloudBase(ChatModelIntegrationTests):
         return True
 
 
-# class TestSambaStudioBase(ChatModelIntegrationTests):
-#     @property
-#     def chat_model_class(self) -> Type[ChatSambaStudio]:
-#         return ChatSambaStudio
+class TestSambaStudioBase(ChatModelIntegrationTests):
+    @property
+    def chat_model_class(self) -> Type[ChatSambaStudio]:
+        return ChatSambaStudio
 
-#     @property
-#     def chat_model_params(self) -> dict:
-#         return {"model": "Meta-Llama-3.3-70B-Instruct", "temperature": 0}
+    @property
+    def chat_model_params(self) -> dict:
+        return {"model": "Meta-Llama-3.3-70B-Instruct", "temperature": 0}
 
-#     @property
-#     def has_tool_calling(self) -> bool:
-#         return True
+    @property
+    def has_tool_calling(self) -> bool:
+        return True
 
-#     @property
-#     def has_structured_output(self) -> bool:
-#         return True
+    @property
+    def has_structured_output(self) -> bool:
+        return True
 
-#     @property
-#     def supports_json_mode(self) -> bool:
-#         return True
+    @property
+    def supports_json_mode(self) -> bool:
+        return True
 
-#     @property
-#     def returns_usage_metadata(self) -> bool:
-#         return True
+    @property
+    def returns_usage_metadata(self) -> bool:
+        return True
