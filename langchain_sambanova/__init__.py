4a21363379e2af7a0c1588223926bd95f4cb7681
snova-jorgep <jorge.piedrahita@sambanovasystems.com>
2025-09-25 18:59:23 -0500

    minor fixes

--- langchain_sambanova/__init__.py
+++ langchain_sambanova/__init__.py
@@ -1,21 +1,14 @@
-from importlib import metadata
-
 from langchain_sambanova.chat_models import ChatSambaNovaCloud, ChatSambaStudio
 from langchain_sambanova.embeddings import (
     SambaNovaCloudEmbeddings,
     SambaStudioEmbeddings,
 )
-
-try:
-    __version__ = metadata.version(__package__)
-except metadata.PackageNotFoundError:
-    # Case where package metadata is not available.
-    __version__ = ""
-del metadata  # optional, avoids polluting the results of dir(__package__)
+from langchain_sambanova.version import __version__
 
 __all__ = [
     "ChatSambaNovaCloud",
     "ChatSambaStudio",
-    "SambaStudioEmbeddings",
     "SambaNovaCloudEmbeddings",
+    "SambaStudioEmbeddings",
+    "__version__",
 ]
